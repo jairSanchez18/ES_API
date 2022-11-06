c8272f2801a6f19dd8e70850c433ff3a3d6f3595
Hazzita/Hazzi <helloimrubenjaen@gmail.com>
2022-11-06 18:42:36 -0500

    salones

---
+++ vistas/salones.php
@@ -0,0 +1,24 @@
+<?php
+
+//DEBEMOS INCLUID LA CONEXION PARA PODER UTILIZAR LA VARIABLE PDO
+include '../config/conexion.php';
+
+////REALIZAMOS LA CONSULTA QUE DESEAMOS
+$consulta = "SELECT * FROM salones";
+
+//OBTENEMOS LA CONSULTA
+$resultado = $pdo->query($consulta);
+
+//RECORREMOS EL SELECT PARA TRAER TODOS LOS DATOS NECESARIOS
+while($fila=$resultado->fetch_array())
+{
+    $prueba[] = array_map('utf8_encode',$fila);
+}
+
+//LO CONVERTIMOS A FORMATO JSON
+echo json_encode ($prueba);
+
+//CERRAMOS TODA LA CONSULTA Y CONEXION
+$resultado->close();
+
+?>
